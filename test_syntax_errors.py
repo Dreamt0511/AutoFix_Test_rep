092e4d9dc6fcbd82df19b335145ecbb47df2a8ac
Dreamt <mochenge@163.com>
2026-04-26 15:05:22 +0800

    test: 添加语法错误测试文件

---
+++ test_syntax_errors.py
@@ -0,0 +1,8 @@
+def process_data(items):
+    result = []
+    for item in items  # 缺少冒号
+        if item > 0:
+            result.append(item * 2)
+        else  # 缺少冒号
+            result.append(item)
+    return result
